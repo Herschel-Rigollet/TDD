454b4c87bb1c5d76d2d869099a364b1afb8c7c49
rigollet <rigollet153@gmail.com>
2025-07-10 21:36:37 +0900

    PointController 구현 및 리턴 타입에 맞춰 PointService 수정

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -14,21 +14,24 @@ public class PointService {
         this.pointHistoryTable = pointHistoryTable;
     }
 
-    public long getPoint(long userId) {
-        return userPointTable.selectById(userId).point();
+    public UserPoint getPoint(long userId) {
+        return userPointTable.selectById(userId);
     }
 
-    public void charge(long userId, long amount) {
+    public UserPoint charge(long userId, long amount) {
         if(amount <= 0) throw new IllegalArgumentException("충전 금액은 0보다 커야 합니다.");
 
         UserPoint current = userPointTable.selectById(userId);
         long newPoint = current.point() + amount;
+        UserPoint updated = new UserPoint(userId, newPoint, System.currentTimeMillis());
 
         userPointTable.insertOrUpdate(userId, newPoint);
         pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, System.currentTimeMillis());
+
+        return updated;
     }
 
-    public void use(long userId, long amount) {
+    public UserPoint use(long userId, long amount) {
         if (amount <= 0) throw new IllegalArgumentException("사용 포인트는 0보다 커야 합니다.");
 
         UserPoint current = userPointTable.selectById(userId);
@@ -36,7 +39,10 @@ public class PointService {
 
         long newPoint = current.point() - amount;
         userPointTable.insertOrUpdate(userId, newPoint);
+        UserPoint updated = new UserPoint(userId, newPoint, System.currentTimeMillis());
         pointHistoryTable.insert(userId, amount, TransactionType.USE, System.currentTimeMillis());
+
+        return updated;
     }
 
     public List<PointHistory> getHistories(long userId) {
